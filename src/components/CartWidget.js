eb705982b904168ff07af92e21f109e1341b6c21
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-04-20 13:32:32 -0300

    Desafio: Crea tu landing

---
+++ src/components/CartWidget.js
@@ -0,0 +1,11 @@
+import { MdOutlineShoppingCart } from 'react-icons/fa';
+
+import { ImCart } from 'react-icons/im';
+
+const CartWidget = () => {
+  return (
+    <a className="bg-dark text-light ml-1 p-1 ms-auto me-3 p-3 fs-3"><ImCart /></a>
+  )
+}
+
+export default CartWidget
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = ({greeting}) => {
+    return (
+      <div clasName="container">
+          <h1 className="p-5 bg-secondary text-light">{greeting}</h1>
+      </div>  
+    )
+}
+
+export default ItemListContainer
